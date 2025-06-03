75eb8d1458f34388d7e0d5a2c487357c60a94dfe
Phạm Đại Trí <125333024+VanderSpeare@users.noreply.github.com>
2025-06-03 12:21:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
-  const fileUrl = `http://localhost:3000/uploads/${req.file.filename}`;
+  const fileUrl = `https://discord-clone-etat.onrender.com/uploads/${req.file.filename}`;
   res.json({ fileUrl });
 });
 
