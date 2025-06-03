77a9dbd7af33510c1ceed6a85e165f941048b957
Phạm Đại Trí <125333024+VanderSpeare@users.noreply.github.com>
2025-06-04 03:59:42 +0700

    Update user.service.js

--- services/user.service.js
+++ services/user.service.js
@@ -64,7 +64,7 @@ async function generateUniqueUsername(displayName) {
 function validateEmail(email) {
   // Cho phép email rỗng nếu đăng ký bằng số điện thoại
   if (!email) return true;
-  const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  const emailRegex = /^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
   return emailRegex.test(email);
 }
 
